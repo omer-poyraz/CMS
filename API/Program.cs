8528720feafe3fb1d5c898b3eb98d2f002621bac
Ömer Poyraz <omer@liberyus.com>
2026-03-04 20:51:38 +0300

    Update configuration settings and modify SQL connection details in appsettings.json

--- API/Program.cs
+++ API/Program.cs
@@ -67,10 +67,12 @@ builder.Services.Configure<GoogleAnalyticsOptions>(
 
 var app = builder.Build();
 
-
+// if (app.Environment.IsDevelopment())
+// {
 app.UseDeveloperExceptionPage();
 app.UseSwagger();
 app.UseSwaggerUI();
+// }
 
 app.UseMiddleware<ContentPrettyFormatMiddleware>();
 app.UseMiddleware<CreateSuperAdminMiddleware>();
